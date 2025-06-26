1c976392cb87e8d54e19278d9b3fe879732febc2
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2025-06-26 23:59:57 +0600

    remove the stats component

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Stats from "@/app/_components/Stats";
 import Topics from "@/app/_components/Topics";
 import Container from "@/components/ui/container";
 import React from "react";
@@ -6,7 +5,6 @@ import React from "react";
 const DashboardPage = () => {
   return (
     <Container className="flex flex-col gap-4">
-      <Stats />
       <Topics />
     </Container>
   );
